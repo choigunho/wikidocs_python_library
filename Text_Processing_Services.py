9683366814a6920b0836bf6c0fb19ad596b91f4b
gunho <choiguenho@gmail.com>
2021-07-09 17:30:12 +0900

    complete chapter1

--- Text_Processing_Services.py
+++ Text_Processing_Services.py
@@ -1,5 +1,5 @@
-import random, itertools
 import textwrap
+import re
 import unittest
 
 '''
@@ -14,6 +14,30 @@ class PrologLists(unittest.TestCase):
 
         self.assertEqual("Life is too...", shorten("Life is too short, you need python"))
         self.assertEqual("Hello World", shorten("Hello World"))
+
+    long_text = 'Life is too short, you need python. ' * 3
+
+    def test_01_02(self):
+        def wrap(long_text):
+            return textwrap.wrap(long_text, width=70)
+
+        self.assertEqual(
+            ['Life is too short, you need python. Life is too short, you need', 'python. Life is too short, you need python.'],
+            wrap(self.long_text)
+        )
+
+    data = """
+    홍길동의 주민번호는 800905-1049118 입니다. 
+    그리고 고길동의 주민번호는 700905-1059119 입니다. 
+    그렇다면 누가 형님일까요?
+    """
+
+    def test_01_03(self):
+        def reg():
+            ex = re.compile('(\d{6})-\d{7}')
+            return ex.sub('\g<1>-*******', self.data)
+
+        print(reg())
 
 
 if __name__ == "__main__":
